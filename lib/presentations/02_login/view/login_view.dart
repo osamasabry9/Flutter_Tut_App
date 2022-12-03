c9f3cd3a41e07011f0f5c7d6620ac8526b0dd261
osamasabry9 <osamasabry324@gmail.com>
2022-12-03 13:47:02 +0200

    Login page (View & View Model)

--- lib/presentations/02_login/view/login_view.dart
+++ lib/presentations/02_login/view/login_view.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+import '../../../core/utils/assets_manager.dart';
+import '../../../core/utils/color_manager.dart';
+import '../../../core/utils/routes_manager.dart';
+import '../../../core/utils/strings_manager.dart';
+import '../../../core/utils/values_manager.dart';
+import '../viewmodel/login_viewmodel.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,8 +16,151 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+
+  final LoginViewModel _viewModel = LoginViewModel();
+
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _userPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  _bind() {
+    _viewModel.start(); // tell view model, start ur job
+    _userNameController
+        .addListener(() => _viewModel.setUserName(_userNameController.text));
+    _userPasswordController.addListener(
+        () => _viewModel.setPassword(_userPasswordController.text));
+
+    _viewModel.isUserLoggedInSuccessfullyStreamController.stream
+        .listen((isLoggedIn) {
+      if (isLoggedIn) {
+        // navigate to main screen
+        SchedulerBinding.instance.addPostFrameCallback((_) {
+          Navigator.of(context).pushReplacementNamed(Routes.mainRoute);
+        });
+      }
+    });
+  }
+
   @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      body: _getContentWidget(),
+    );
+  }
+
+    Widget _getContentWidget() {
+    return Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Center(
+                    child: Image(image: AssetImage(ImageAssets.splashLogo))),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outIsUserNameValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _userNameController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.username,
+                              labelText: AppStrings.username,
+                              errorText: (snapshot.data ?? true)
+                                  ? null
+                                  : AppStrings.usernameError),
+                        );
+                      }),
+                ),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outIsPasswordValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: _userPasswordController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.password,
+                              labelText: AppStrings.password,
+                              errorText: (snapshot.data ?? true)
+                                  ? null
+                                  : AppStrings.passwordError),
+                        );
+                      }),
+                ),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outAreAllInputsValid,
+                      builder: (context, snapshot) {
+                        return SizedBox(
+                          width: double.infinity,
+                          height: AppSize.s40,
+                          child: ElevatedButton(
+                              onPressed: (snapshot.data ?? false)
+                                  ? () {
+                                      _viewModel.login();
+                                    }
+                                  : null,
+                              child: const Text(AppStrings.login)),
+                        );
+                      }),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(
+                        top: AppPadding.p8,
+                        left: AppPadding.p28,
+                        right: AppPadding.p28),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, Routes.forgotPasswordRoute);
+                          },
+                          child: Text(AppStrings.forgetPassword,
+                              style: Theme.of(context).textTheme.titleMedium),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, Routes.registerRoute);
+                          },
+                          child: Text(AppStrings.registerText,
+                              style: Theme.of(context).textTheme.titleMedium),
+                        )
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ));
+  }
+    @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
---
+++ lib/presentations/02_login/viewmodel/login_viewmodel.dart
@@ -0,0 +1,128 @@
+import 'dart:async';
+
+import 'package:clean_architecture_with_mvvm/presentations/000_base/base_view_model.dart';
+
+import '../../../core/common/freezed_data_classes.dart';
+import '../../../domain/usecase/login_usecase.dart';
+
+class LoginViewModel extends BaseViewModel
+    with LoginViewModelInputs, LoginViewModelOutputs {
+  final StreamController _userNameStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _passwordStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _areAllInputsValidStreamController =
+      StreamController<void>.broadcast();
+
+  StreamController isUserLoggedInSuccessfullyStreamController =
+      StreamController<bool>();
+
+  var loginObject = LoginObject("", "");
+  //final LoginUseCase _loginUseCase;
+
+  //LoginViewModel(this._loginUseCase);
+  LoginViewModel();
+
+  @override
+  void dispose() {
+    _userNameStreamController.close();
+    _passwordStreamController.close();
+    _areAllInputsValidStreamController.close();
+    isUserLoggedInSuccessfullyStreamController.close();
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+    
+  }
+
+  @override
+  Sink get inputUserName => _userNameStreamController.sink;
+
+  @override
+  Sink get inputPassword => _passwordStreamController.sink;
+
+  @override
+  Sink get inputAreAllInputsValid => _areAllInputsValidStreamController.sink;
+
+  @override
+  setUserName(String userName) {
+    inputUserName.add(userName);
+    loginObject = loginObject.copyWith(userName: userName);
+    inputAreAllInputsValid.add(null);
+  }
+
+  @override
+  setPassword(String password) {
+    inputPassword.add(password);
+    loginObject = loginObject.copyWith(password: password);
+    inputAreAllInputsValid.add(null);
+  }
+
+  @override
+  login() async {
+    // (await _loginUseCase.execute(
+    //         LoginUseCaseInput(loginObject.userName, loginObject.password)))
+    //     .fold(
+    //         (failure) => {
+    //               // left -> failure
+    //             }, (data) {
+    //   // right -> data (success)
+    //   // content
+    //   // navigate to main screen
+    //   isUserLoggedInSuccessfullyStreamController.add(true);
+    // });
+  }
+
+  // outputs
+  @override
+  Stream<bool> get outIsPasswordValid => _passwordStreamController.stream
+      .map((password) => _isPasswordValid(password));
+
+  @override
+  Stream<bool> get outIsUserNameValid => _userNameStreamController.stream
+      .map((userName) => _isUserNameValid(userName));
+
+  @override
+  Stream<bool> get outAreAllInputsValid =>
+      _areAllInputsValidStreamController.stream
+          .map((_) => _areAllInputsValid());
+
+  bool _isPasswordValid(String password) {
+    return password.isNotEmpty;
+  }
+
+  bool _isUserNameValid(String userName) {
+    return userName.isNotEmpty;
+  }
+
+  bool _areAllInputsValid() {
+    return _isPasswordValid(loginObject.password) &&
+        _isUserNameValid(loginObject.userName);
+  }
+}
+
+abstract class LoginViewModelInputs {
+  setUserName(String userName);
+
+  setPassword(String password);
+
+  login();
+
+  Sink get inputUserName;
+
+  Sink get inputPassword;
+
+  Sink get inputAreAllInputsValid;
+}
+
+abstract class LoginViewModelOutputs {
+  Stream<bool> get outIsUserNameValid;
+
+  Stream<bool> get outIsPasswordValid;
+
+  Stream<bool> get outAreAllInputsValid;
+}
